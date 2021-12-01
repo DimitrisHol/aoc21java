d1ce118c8dc81585d409f4745252c0a67f76a384
Dimitris Hol <dimitrishol@protonmail.com>
2021-12-02 00:42:11 +0200

    Day01

---
+++ src/day01/Day01.java
@@ -0,0 +1,74 @@
+package day01;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day01 {
+
+    public static void main(String[] args) throws IOException {
+
+        List<String> records = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File("src/day01/input01.txt"))) {
+//        try (Scanner scanner = new Scanner(new File("src/day01/testInput01.txt"))) {
+            while (scanner.hasNextLine()) {
+                records.add(scanner.next());
+            }
+        }
+
+        part1(records);
+        part2(records);
+
+    }
+
+    private static void part1(List<String> records) {
+        int previousDepth = 0;
+        int counterDepth = 0;
+        for (String record : records) {
+
+
+            int currentDepth = Integer.parseInt(record);
+            if (currentDepth > previousDepth) {
+                counterDepth++;
+            }
+            previousDepth = currentDepth;
+        }
+
+        System.out.println(counterDepth -1 );
+    }
+
+    private static void part2(List<String> records) {
+
+        int previousWindowSum = 0;
+        int counterDepth = 0;
+
+        List<Integer> depthWindow = new ArrayList<>();
+
+        for (String record : records) {
+
+            int currentDepth = Integer.parseInt(record);
+
+            if (depthWindow.size() < 3) {
+                depthWindow.add(currentDepth);
+            }
+
+            if (depthWindow.size() == 3) {
+
+                int currentWindowSum = depthWindow.stream().reduce(0, Integer::sum);
+                if (currentWindowSum > previousWindowSum) {
+                    counterDepth++;
+                }
+                depthWindow.remove(0);
+                previousWindowSum = currentWindowSum;
+            }
+
+        }
+        System.out.println(counterDepth -1 );
+    }
+
+}
+
+
+
